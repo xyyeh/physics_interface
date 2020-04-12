6a8144a5a62615691cf8d22d1ce3390242a7de92
Xiyang Yeh <xiyang.yeh@flexivrobotics.com>
2020-04-11 22:23:44 -0700

    Added stewart

--- thirdparty/build_bullet.sh
+++ thirdparty/build_bullet.sh
@@ -9,8 +9,13 @@ cmake .. -DBUILD_PYBULLET=OFF -DBUILD_UNIT_TESTS=OFF $@
 make -j8
 make install
 
+cd ..
 cp build/examples/RobotSimulatorShm/RobotSimulatorShm ../../app_simulation/
 cp build/data/checker_blue.png ../../app_simulation/
 cp build/data/plane.mtl ../../app_simulation/
 cp build/data/plane.obj ../../app_simulation/
 cp build/data/plane.urdf ../../app_simulation/
+cp -rf build/data/panda ../../app_simulation/
+
+cp build/examples/RobotSimulatorStewart/RobotSimulatorStewart ../../app_simulation/
+cp -rf build/data/stewart ../../app_simulation/
